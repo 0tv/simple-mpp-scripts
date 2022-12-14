80f0ffeec0592e34e980c37939f8c1d6fad54052
Leaf ヅ <62675630+0tv@users.noreply.github.com>
2022-12-14 12:19:57 +0000

    Create cosmic-autobake.js
    work in progress

---
+++ automation/cosmic-autobake.js
@@ -0,0 +1,45 @@
+/*
+  Cosmic is a multi-purpose, outer space-themed bot on multiplayerpiano, created by Hri7566.
+  This file automates using the baking comman efficiently and without the need for the user to interact.
+  
+  It is generally recommended that you keep the tab focused in some way so that it does not periodically disconnect, while this is not usually an issue, there can be cases where the script misses a chat message from the bot due to being disconnected, which breaks the baking loop and requires manually restarting. If you suspect you may disconnect at times, it is recommended to set the BAKE_ON_RECONNECT boolean to true.
+*/
+
+// constants
+const BAKE_ON_RECONNECT = true; // whether or not to send the bake command after being disconnected and reconnected.
+const BAKE_ON_EXECUTE = true; // Whether or not to immediately send the bake command in chat when the script is ran.
+const BOT_IDS = [ "5fd68beb48a7cca92bc536c0" ]; // _IDs of the cosmic bot, leave as-is if you are unsure. If empty, all _IDs are accepted as being the bot.
+const BOT_PREFIX = '*'; // The prefix for the bot, this is the character or string that comes before commands (e.g. *help)
+const MESSAGE_PREFIX = '\u034f'; // Anything that comes before the actual message in chat, leave as-is if you are unsure.
+const BAKING_MESSAGE = "[Wawa's baking script]"; // Text that comes after the baking command, this input is not required and is ignored by the bot.
+const DEBUG = false; // Set to true if you want to see the script's thought process in your console.
+
+// functions
+function handleMessage(event) {
+  // get useful details from the event object
+  let user = event.p;
+  let message = event.a;
+  let timestamp = new Date(event.t);
+  
+  if(BOT_IDS.length === 0 || BOT_IDS.includes(user._id)) {
+    // This user is the cosmic bot, proceed with checking message.
+    if(message.startsWith(MESSAGE_PREFIX + ) {
+
+    };
+  }; 
+};
+
+function debug(service, text) {
+  if(DEBUG) console.log("[COSMIC-AUTOBAKE.JS] [DEBUG:" + service + "]" + text);
+};
+
+// listeners
+MPP.client.on('a', handleMessage);
+MPP.client.on('ch', event => {
+  if(BAKE_ON_RECONNECT) {
+    debug("BakeOnReconnect", "Sending chat message to bake.");
+    MPP.chat.send(BOT_PREFIX + 'bake');
+  } else {
+    debug("BakeOnReconnect", "Not baking.");
+  };
+});
